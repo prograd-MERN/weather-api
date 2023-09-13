e380d6ca7ab5c2f2e7523c302c55d6e551a46e38
Balaramansethu <baalaaraman@gmail.com>
2023-09-13 16:01:10 +0530

    Initial commit , for file structure.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherAppComponent from './components/WeatherAppComponent/WeatherAppComponent'
+
+const App = () => {
+  return (
+      <React.Fragment>
+        <WeatherAppComponent/>
+      </React.Fragment>
+    )
+}
+
+export default App
---
+++ src/components/WeatherAppComponent/WeatherAppComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'tachyons'
+const WeatherAppComponent = () => {
+  return (
+        <React.Fragment>
+            <h1 className='tc ' >Weather-Api</h1>
+        </React.Fragment>
+    )
+}
+
+export default WeatherAppComponent
